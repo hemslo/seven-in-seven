fe1afe3b25209433848948209cb5834cfbc14a78
Hemslo Wang <hemslo.wang@gmail.com>
2015-12-08 00:31:56 +0800

    Add prolog day 1

---
+++ prolog/day1/day1.pl
@@ -0,0 +1,22 @@
+% Make a simple knowledge base. Represent some of your favorite books and authors.
+% Find all books in your knowledge base written by one author.
+
+writes(alice, a).
+writes(alice, l).
+writes(alice, i).
+writes(alice, c).
+writes(alice, e).
+writes(bob, b).
+writes(bob, o).
+
+% wirtes(alice, Book).
+
+
+% Make a knowledge base representing musicians and instruments. Also represent musicians and their genre of music.
+% Find all musicians who play the guitar.
+
+musician_instrument(alice, guitar).
+musician_instrument(bob, guitar).
+musician_genre(alice, a).
+
+% musician_instrument(Musician, guitar).
