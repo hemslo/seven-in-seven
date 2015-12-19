3c7025b538af931846e328d2d27d5d5bd4f9a86e
Hemslo Wang <hemslo.wang@gmail.com>
2015-12-19 09:53:07 +0800

    Use supervisor in erlang OTP

--- erlang/day3/translate_service.erl
+++ erlang/day3/translate_service.erl
@@ -1,5 +1,8 @@
+% Monitor the translate_service and restart it should it die.
+
 -module(translate_service).
--export([loop/0, translate/2]).
+-behaviour(supervisor).
+-export([loop/0, translate/2, start/0, init/1, start_service/0]).
 
 loop() ->
   receive
@@ -17,7 +20,29 @@ loop() ->
   end.
 
 translate(To, Word) ->
-    To ! {self(), Word},
-    receive
-        Translation -> Translation
-    end.
+  To ! {self(), Word},
+  receive
+    Translation -> Translation
+  end.
+
+start() ->
+  io:fwrite("Starting...~n"),
+  Pid = spawn_link(fun loop/0),
+  register(translator, Pid),
+  {ok, Pid}.
+
+init(_) ->
+  { ok, { { one_for_one, 1, 60 },
+    [{ translate_service,
+       { translate_service, start, [] },
+       permanent,
+       brutal_kill,
+       worker,
+       [translate_service]
+     }
+    ]}
+  }.
+
+start_service() ->
+  io:fwrite("start_service~n"),
+  supervisor:start_link(translate_service, []).
