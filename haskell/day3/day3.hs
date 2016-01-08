5312fb71fde445bdf42fd654c2d2267312ebb8d0
Hemslo Wang <hemslo.wang@gmail.com>
2016-01-08 10:12:22 +0800

    Add haskell day3 maze represent

--- haskell/day3/day3.hs
+++ haskell/day3/day3.hs
@@ -1,7 +1,14 @@
 module Main where
+  import Data.List
 -- • Write a function that looks up a hash table value that uses the Maybe monad. Write a hash that stores other hashes, several levels deep. Use the Maybe monad to retrieve an element for a hash key several levels deep.
   try :: Eq a => a -> [(a, b)] -> Maybe b
   try _ [] = Nothing
   try key ((k, v):t)
     | key == k = Just v
     | otherwise = try key t
+
+-- • Represent a maze in Haskell. You’ll need a Maze type and a Node type, as well as a function to return a node given its coordinates. The node should have a list of exits to other nodes.
+  type Position = (Int, Int)
+  type Node = (Position, [Position])
+  type Maze = [Node]
+  locate position maze = find (\(pos, _) -> pos == position) maze
