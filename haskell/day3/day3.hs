5f2b9b52757276c417880f61963a1d2fe257a984
Hemslo Wang <hemslo.wang@gmail.com>
2016-01-09 23:39:19 +0800

    Add haskell day 3 maze solve

--- haskell/day3/day3.hs
+++ haskell/day3/day3.hs
@@ -9,6 +9,30 @@ module Main where
 
 -- • Represent a maze in Haskell. You’ll need a Maze type and a Node type, as well as a function to return a node given its coordinates. The node should have a list of exits to other nodes.
   type Position = (Int, Int)
-  type Node = (Position, [Position])
+  data Node = Node Position [Position] deriving Show
   type Maze = [Node]
-  locate position maze = find (\(pos, _) -> pos == position) maze
+  locate :: Maze -> Position -> Maybe Node
+  locate maze position = find (\(Node pos _) -> pos == position) maze
+
+-- • Use a List monad to solve the maze.
+  exits Nothing = []
+  exits (Just (Node _ exits)) = exits
+
+  solve :: Maze -> Position -> Position -> Maybe [Position]
+  solve maze start exit =
+    case tryMoves [start] of
+      [] -> Nothing
+      [path] -> Just(reverse path)
+    where
+      tryMoves path@(pos:_)
+        | pos == exit = return path
+        | otherwise = do
+            exit <- (exits(locate maze pos)) \\ path
+            tryMoves (exit:path)
+
+-- let maze = [(Node (0, 0) [(0, 1)]), (Node (0, 1) [(0, 0), (1, 1)]),
+--             (Node (1, 0) []), (Node (1, 1) [(0, 1)])]
+-- solve maze (0, 0) (1, 1)
+--   Just [(0,0),(0,1),(1,1)]
+-- solve maze (0, 0) (1, 0)
+--   Nothing
