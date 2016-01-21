093c5f31453fe3223c67ffff05f712afe4507307
Hemslo Wang <hemslo.wang@gmail.com>
2016-01-22 00:23:48 +0800

    Fix elm compile error

--- elm/day2/hard.elm
+++ elm/day2/hard.elm
@@ -9,33 +9,34 @@ import Mouse
 import Window
 import Time exposing (Time, fps, inSeconds)
 
-carBottom = filled black (rect 160 50)
-carTop = filled black (rect 100 60)
-tire = filled red (circle 24)
-
 type alias Input = { w:Int, x:Int, delta:Time }
 type alias Car = { x:Float, forward:Bool, vx:Float}
+
+defaultCar = {x=300.0, forward=True, vx=1.0}
 
 secsPerFrame = 1.0 / 50.0
 delta = Signal.map inSeconds (fps 50)
 input = sampleOn delta (Signal.map3 Input Window.width Mouse.x delta)
 
-defaultCar = {x=300.0, forward=True, vx=1.0}
+main = map display (foldp step defaultCar input)
 
+step : Input -> Car -> Car
 step input ({x, forward, vx} as car) =
-   let forward' = if (car.x > ((input.w * 2) - 150)) || (car.x < 150)
-                  then not (car.forward)
-                  else car.forward
-       vx' = 50 + toFloat input.x / 100.0
+   let forward' = if (x > ((toFloat input.w * 2) - 150)) || (x < 150)
+                  then not forward
+                  else forward
+       vx' = 50.0 + toFloat input.x
        distance = vx' * secsPerFrame
    in { car | x = x + if forward' then distance else -distance, forward = forward', vx = vx' }
 
 display car =
-  collage (car.x) 300
+  collage (round car.x) 300
   [ carBottom
   , carTop |> moveY 30
   , tire |> move (-40, -28)
   , tire |> move (40, -28)
   ]
 
-main = map display (foldp step defaultCar input)
+carBottom = filled black (rect 160 50)
+carTop = filled black (rect 100 60)
+tire = filled red (circle 24)
